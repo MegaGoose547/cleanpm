cb42d88ac9ca3f02f16a4ec9cc085452422216a4
anonygoose <100034844+MegaGoose547@users.noreply.github.com>
2025-07-31 14:22:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let sortNewestFirst = true;
 const supabaseUrl = 'https://ubrsdotiwcmiyznvzhcw.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVicnNkb3Rpd2NtaXl6bnZ6aGN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5ODEwMzksImV4cCI6MjA2OTU1NzAzOX0.n5p9dxhNv5u7GyvJ3e2xignsJDyOcBjsFWV6FCwxeVQ';
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 async function loadGallery() {
   const gallery = document.getElementById('gallery');
